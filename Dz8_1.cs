f099165735f0d10b111e40bd2ee099949747e036
Oleksandr Vialtsev <oleksandr.vialtsev.student@knu.ua>
2023-11-27 10:03:46 +0200

    Добавьте файлы проекта.

---
+++ Dz8_1.cs
@@ -0,0 +1,36 @@
+﻿namespace Dz8_1;
+
+class Item
+{
+}
+class Order
+{
+    private List<Item> itemList;
+
+    internal List<Item> ItemList
+    {
+        get { return itemList; }
+        set { itemList = value; }
+    }
+    public void AddItem() {/*...*/}
+    public void DeleteItem() {/*...*/}
+    public void GetItems() {/*...*/}
+}
+class OrderCalculator
+{
+    public void CalculateTotalSum() {/*...*/}
+    public void GetItemCount() {/*...*/}
+}
+class OrderVisualization
+{
+    public void PrintOrder() {/*...*/}
+    public void ShowOrder() {/*...*/}
+}
+
+class Manager
+{
+    public void Load() {/*...*/}
+    public void Save() {/*...*/}
+    public void Update() {/*...*/}
+    public void Delete() {/*...*/}
+}
